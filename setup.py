21096be735508e998a150b6b13c81551e87bead0
TheRealKeto <therealketo@gmail.com>
2020-08-09 01:33:02 -0400

    Add the ability to install the wrapper through pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requirements = []
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+setup(
+    name = "trackrr.py",
+    version = "0.0.1"
+    author = "ms7m",
+    description = "An asynchronous Python API wrapper for Trackrr.",
+    long_description = long_desc,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/TheRealKeto/Trackrr.py",
+    packages = ["trackrr_py"],
+    install_requires = requirements,
+    python_requires = ">=3.5.3"
+)
