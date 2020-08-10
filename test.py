df92a637f79a2049067697ec4347ec7118623d74
TheRealKeto <therealketo@gmail.com>
2020-08-09 23:33:54 -0400

    Change our example file to reflect recent changes
    This reflects recent changes and provides a more, in-deph, example.

--- test.py
+++ test.py
@@ -1,6 +1,31 @@
-from trackrr_py import Trackrr
+# Trackrr.py (test.py)
+# Trackkr.py Example
 
+# Imports
+import asyncio
+from trackrr.client import Trackrr
 
-j = Trackrr(api_key="4c43b008-afdc-4b2a-a706-f41072ae2cfa")
+# Create an async function
+async def main():
+    """ Searchs for a song using the Trackrr API """
+    # This function returns more than 1 result
+    # TODO: Return one result only, if possible
 
-o = j.search_song_by_query("Valentino")
+    # Specify the Trackrr client
+    # You must specify an API key to this client
+    client = Trackrr("API Key here")
+    # Search Trackrr for a song based on a given query
+    query = await client.search_song("Valentino")
+    # Select the first result from the query
+    # Print its contents (check the docs for other values)
+    result = query[0]
+    print(f"Song Name: {result.track_name}")
+    print(f"Artists: {result.track_artists}")
+    print(f"Song URL: {result.track_url}")
+    # Close our Trackrr client
+    # Always do this after each request
+    await client.close()
+
+# Run the function above (asynchronously)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
